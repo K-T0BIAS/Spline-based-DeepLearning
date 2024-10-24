54f1b850bb487c6d403cbfe11328d7b4c3497a28
K-T0BIAS <karusseittobi@gmail.com>
2024-10-24 22:41:02 +0200

    stetup testing prerequisits

--- tests/unit_tests/layer_tests.cpp
+++ tests/unit_tests/layer_tests.cpp
@@ -1,4 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
 #include "../include/SplineNetLib/layers.hpp"
 
 using namespace SplineNetLib ;
 
+//test initialization
+TEST_CASE("layer initialization using constructor method functions as expected") {}
+    
+}
