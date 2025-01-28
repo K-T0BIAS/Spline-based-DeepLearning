ba3c0530569043d9f1a48f3c027fb496306496ba
K-T0BIAS <karusseittobi@gmail.com>
2025-01-28 12:00:44 +0100

    added new test for spline interpolation correctness.

--- tests/unit_tests/spline_tests.cpp
+++ tests/unit_tests/spline_tests.cpp
@@ -25,6 +25,7 @@ TEST_CASE("spline initialization using constructor method functions as expected"
 }
 
 TEST_CASE("spline interpolation functions as expected"){
+    //these are the reference points and params that the spline shouod interpolate to
     std::vector<std::vector<double>> correct_points = {{0.0,0.0},{0.2,1.0},{0.4,2.5},{0.6,2.0},{0.8,2.0},{1.0,0.5}};
     std::vector<std::vector<double>> correct_parameters (5, std::vector<double>(4,0.0));
     
@@ -32,4 +33,54 @@ TEST_CASE("spline interpolation functions as expected"){
     Test_spline.interpolation();
     std::vector<std::vector<double>> spline_points = Test_spline.get_points();
     std::vector<std::vector<double>> spline_parameters = Test_spline.get_params();
+    
+    //check if spline points and correct_points. if print both
+    if (spline_points != correct_points){
+        std::cout<<"spline points after interpolation do not match expected points: \n";
+        std::cout<<"correct points = (";
+        for (size_t i = 0; i < correct_points.size(); i++){
+            std::cout<<"(";
+            for size_t j = 0: j < correct_points[0].size(); i++){
+                std::cout<<correct_points[i][j];
+                (j < correct_points[0].size()-1) ? std::cout<<", " : std::cout<<")"
+            }
+            (i < correct_points.size()-1)? std::cout<<"), ": std::cout<<")\n";
+        }
+        
+        std::cout<<"spline points = (";
+        for (size_t i = 0; i < spline_points.size(); i++){
+            std::cout<<"(";
+            for size_t j = 0: j < spline_points[0].size(); i++){
+                std::cout<<spline_points[i][j];
+                (j < spline_points[0].size()-1) ? std::cout<<", " : std::cout<<")"
+            }
+            (i < spline_points.size()-1)? std::cout<<"), ": std::cout<<")\n";
+        }
+    }
+    REQUIRE(spline_points == correct_points);
+    
+    if (spline_parameters != correct_parameters){
+        std::cout<<"spline parameters after interpolation do not match expected parameters: \n";
+        std::cout<<"correct parameters = (";
+        for (size_t i = 0; i < correct_parameters.size(); i++){
+            std::cout<<"(";
+            for size_t j = 0: j < correct_parameters[0].size(); i++){
+                std::cout<<correct_parameters[i][j];
+                (j < correct_parameters[0].size()-1) ? std::cout<<", " : std::cout<<")"
+            }
+            (i < correct_parameters.size()-1)? std::cout<<"), ": std::cout<<")\n";
+        }
+        
+        std::cout<<"spline parameters = (";
+        for (size_t i = 0; i < spline_parameters.size(); i++){
+            std::cout<<"(";
+            for size_t j = 0: j < spline_parameters[0].size(); i++){
+                std::cout<<spline_parameters[i][j];
+                (j < spline_parameters[0].size()-1) ? std::cout<<", " : std::cout<<")"
+            }
+            (i < spline_parameters.size()-1)? std::cout<<"), ": std::cout<<")\n";
+        }
+    }
+    REQUIRE(spline_parameters == correct_parameters);
 }
+
